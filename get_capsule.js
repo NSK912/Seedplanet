2f6f09b5ec46e8c309865b1519a848864ef91bea
Nat <nang105447@outlook.com>
2026-09-03 16:21:48 +0700

    Delete get_capsule.js

--- get_capsule.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('./public/gamefile/gameplay/player.js', 'utf8');
-const match = code.match(/function generateCapsule[\s\S]*?return \{ vertices, normals, indices \};\n      \}/);
-if (match) console.log(match[0]);
